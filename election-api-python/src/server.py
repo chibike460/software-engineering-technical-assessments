ea3f0359418d148e1ea80b1c237b8cd0eee01106
Samuel George <georgesam.iv@gmail.com>
2024-06-23 19:55:42 +0100

    Setup MOck Data

--- election-api-python/src/server.py
+++ election-api-python/src/server.py
@@ -1,8 +1,34 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from results_controller import ResultsController
+import os
+import json
+
 
 app: Flask = Flask(__name__)
 controller: ResultsController = ResultsController()
+
+
+# Define the directory containing the JSON files
+directory = 'resources/sampleelectionresults/'
+
+
+
+# Iterate over each file in the directory
+for filename in os.listdir(directory):
+    if filename.endswith('.json'):
+        filepath = os.path.join(directory, filename)
+        with open(filepath, 'r') as json_file:
+            # Load the JSON content and append it to the list
+            content = json.load(json_file)
+            # pprint.pprint(content)
+            controller.new_result(content)
+
+
+
+
+@app.route("/")
+def index():
+    return jsonify({"message": "Homepage"})
 
 @app.route("/result/<id>", methods=["GET"])
 def individual_result(id) -> dict:
